c477dfedda6e5e687442217a5f321ebbfb313e5f
fan.ath <fanouria.ath@gmail.com>
2021-05-14 18:30:43 +0300

    new message

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("If you see this message, then the build went successfully");
         System.out.println("At " + LocalDateTime.now());
-    	System.out.println("If you see this message, then the build went successfully");}
+    	System.out.println("If you see this message, then the build went successfully");
+    }
 }
